840cb8c792dc4240d0381f0f3496a2abc492e7e3
yejiCho <goe152@naver.com>
2020-02-11 00:04:48 +0900

    [add]django_settings

---
+++ fc_community/fcuser/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Fcuser
+
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = ('username','password')
+
+admin.site.register(Fcuser, FcuserAdmin)
