132fea91831563b50ed785fffaf25985a34907db
yejiCho <goe152@naver.com>
2020-02-13 03:13:28 +0900

    [add] JoinUs

---
+++ fc_community/fcuser/urls.py
@@ -0,0 +1,10 @@
+# from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    # http://127.0.0.1:8000/fcuser/register/
+    path('register/', views.register),
+
+    # path('admin/', admin.site.urls),
+    # path('fcuser/', include('fcuser.urls'))
+]
--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,3 +1,46 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from .models import Fcuser
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    # folder/folder/register.html
+    elif request.method == 'POST':
+        # username = request.POST['username']
+        # password = request.POST['password']
+        # passwordcheck = request.POST['passwordcheck']
+        username = request.POST.get('username',None)
+        password = request.POST.get('password', None)
+        passwordcheck = request.POST.get('passwordcheck',None)
+        
 
-# Create your views here.
+        res_data = {}
+
+        if not (username and password and passwordcheck):
+            res_data['error'] = 'input all data'
+        elif password != passwordcheck:
+            res_data["error"] = "not match passwrod"
+            # return HttpResponse('not match password')
+        else:
+            fcuser = Fcuser(
+            username = username,
+            # password = password
+            password = make_password(password)
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
+
+        # if password != passwordcheck:
+        #     return HttpResponse('not match password')
+
+        # fcuser = Fcuser(
+        #     username = username,
+        #     password = password
+        # )
+
+        # fcuser.save()
+
+        # return render(request, 'register.html')
